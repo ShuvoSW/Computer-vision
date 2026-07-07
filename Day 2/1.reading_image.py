10eaf49dd400055b721bbd304403e345ee9d44a2
Shuvo Majumder <shuvomajumder3369@gmail.com>
2026-07-07 15:45:08 +0600

    Reading from webcam

--- Day 2/1.reading_image.py
+++ Day 2/1.reading_image.py
@@ -10,14 +10,28 @@ import cv2
 
 ## Reading videos
 
-cap = cv2.VideoCapture("resources/elon.mp4")
-print(cap)
+# cap = cv2.VideoCapture("resources/elon.mp4")
+# print(cap)
+
+# while True:
+#     success, img = cap.read()
+#     print(img.shape)
+#     cv2.imshow("Output", img)
+    
+#     if cv2.waitKey(1) & 0xff == ord('q'):
+#         break
+    
+## Reading from webcam
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)  # width
+cap.set(4, 480)  # hight
 
 while True:
     success, img = cap.read()
-    print(img.shape)
     cv2.imshow("Output", img)
     
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
-    
+        
