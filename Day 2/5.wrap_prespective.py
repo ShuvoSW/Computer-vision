aea13e2afce07b2a6cf75d75e7340c492faf3a25
Shuvo Majumder <shuvomajumder3369@gmail.com>
2026-07-07 19:27:43 +0600

    Practice

--- Day 2/5.wrap_prespective.py
+++ Day 2/5.wrap_prespective.py
@@ -3,9 +3,11 @@ import numpy as np
 
 width , height = 250, 350
 
-img = cv2.imread("resources/cards.jpg")
+# img = cv2.imread("resources/cards.jpg")
+img = cv2.imread("resources/docs.jpg")
 
-pts1 = np.float32([[752,188],[1120,265],[540,668],[871,830]])
+# pts1 = np.float32([[752,188],[1120,265],[540,668],[871,830]])
+pts1 = np.float32([[317,270],[731,268],[222,865],[728,864]])
 pts2 = np.float32([[0,0], [width,0], [0,height], [width,height]]) # Formulaff
 
 metrix = cv2.getPerspectiveTransform(pts1, pts2)
