4b12cb3329a3a17065b2262771b4c0b0d0d8e825
Shuvo Majumder <shuvomajumder3369@gmail.com>
2026-07-07 18:12:25 +0600

    Croping images

--- Day 2/3. resize_image.py
+++ Day 2/3. resize_image.py
@@ -1,10 +1,17 @@
 import cv2
 
 ### 1. Resizing images
+# img = cv2.imread("resources/lambo.png")
+# print(img.shape)
+# resized_img = cv2.resize(img, (300, 200))
+# print(resized_img.shape)
+# cv2.imshow("Output", img)
+# cv2.imshow("Resized_Output", resized_img)
+# cv2.waitKey(0)
+
+### 1. Croping images
 img = cv2.imread("resources/lambo.png")
-print(img.shape)
-resized_img = cv2.resize(img, (300, 200))
-print(resized_img.shape)
+crop_img = img[0:200,200:500]
 cv2.imshow("Output", img)
-cv2.imshow("Resized_Output", resized_img)
+cv2.imshow("Crop_Output", crop_img)
 cv2.waitKey(0)
