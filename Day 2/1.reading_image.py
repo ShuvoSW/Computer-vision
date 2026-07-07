bc0fe462ecead8d94e235b3e6de88950c080c321
Shuvo Majumder <shuvomajumder3369@gmail.com>
2026-07-07 15:32:38 +0600

    Reading videos

--- Day 2/1.reading_image.py
+++ Day 2/1.reading_image.py
@@ -2,8 +2,22 @@ import cv2
 
 ## Reading images
 
-img = cv2.imread("resources/lena.png")
+# img = cv2.imread("resources/lena.png")
 
-print(img.shape)
-cv2.imshow("Output", img)
-cv2.waitKey(0)
+# # print(img.shape)
+# cv2.imshow("Output", img)
+# cv2.waitKey(0)
+
+## Reading videos
+
+cap = cv2.VideoCapture("resources/elon.mp4")
+print(cap)
+
+while True:
+    success, img = cap.read()
+    print(img.shape)
+    cv2.imshow("Output", img)
+    
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+    
