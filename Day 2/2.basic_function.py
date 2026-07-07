424509acb6dd20599405b03eb032368559c6af5b
Shuvo Majumder <shuvomajumder3369@gmail.com>
2026-07-07 17:58:49 +0600

    convet to canny image

--- Day 2/2.basic_function.py
+++ Day 2/2.basic_function.py
@@ -12,15 +12,32 @@ import cv2
 # cv2.waitKey(0)
 
 ### 1. convert to blur image
+# img = cv2.imread("resources/lena.png")
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img_blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
+
+# print(img.shape)
+# print(img_gray.shape)
+# print(img_blur.shape)
+
+# cv2.imshow("Original Image", img)
+# cv2.imshow("Grayscale Image", img_gray)
+# cv2.imshow("Blurred Image", img_blur)
+# cv2.waitKey(0)
+
+### 1. convert to canny image
 img = cv2.imread("resources/lena.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
+img_canny = cv2.Canny(img_blur, 100, 100)
 
 print(img.shape)
 print(img_gray.shape)
 print(img_blur.shape)
+print(img_canny.shape)
 
 cv2.imshow("Original Image", img)
 cv2.imshow("Grayscale Image", img_gray)
 cv2.imshow("Blurred Image", img_blur)
+cv2.imshow("Canny Image", img_canny)
 cv2.waitKey(0)
